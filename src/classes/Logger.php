b11d6a4a1742eb16eb79f2cea097bddb89566940
Mistralys <s.mordziol@gmail.com>
2019-10-20 17:17:40 +0200

    Started adding log modes.

--- src/classes/Logger.php
+++ src/classes/Logger.php
@@ -6,8 +6,17 @@ namespace Microsite;
 
 class Logger
 {
+    const MODE_NONE = 'none';
+    const MODE_ECHO = 'echo';
+    
+    protected static $logMode = self::MODE_NONE;
+    
     public static function log(string $message, string $prefix='') : void
     {
+        if(self::$logMode === self::MODE_NONE) {
+            return;
+        }
+        
         if(is_array($message)) 
         {
             $message = nl2br(json_encode($message));
@@ -17,13 +26,19 @@ class Logger
             $prefix .= ' | ';
         }
         
-        echo '<div style="font-family:monospace;">'.date('i:s').' | '.$prefix.$message.'</div>';
+        if(self::$logMode === self::MODE_ECHO) {
+            echo '<div style="font-family:monospace;">'.date('i:s').' | '.$prefix.$message.'</div>';
+        }
     }
     
     protected static $separator;
     
     public static function logHeader(string $header) : void
     {
+        if(self::$logMode === self::MODE_NONE) {
+            return;
+        }
+        
         if(!isset(self::$separator)) {
             self::$separator = str_repeat('-', 70);
         }
