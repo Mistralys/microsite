8159020848e242a8b5eef49cdbe5825c9c4d928a
Mistralys <s.mordziol@gmail.com>
2019-10-29 10:07:13 +0100

    Added the internal translation functions.

--- src/functions.php
+++ src/functions.php
@@ -16,3 +16,18 @@ function nextJSID() : string
     
     return 'E'.$counter;
 }
+
+function t()
+{
+    return call_user_func_array('\AppLocalize\t', func_get_args());
+}
+
+function pt()
+{
+    return call_user_func_array('\AppLocalize\pt', func_get_args());
+}
+
+function pts()
+{
+    return call_user_func_array('\AppLocalize\pts', func_get_args());
+}
